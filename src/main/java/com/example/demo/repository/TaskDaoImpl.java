7259b5eada3118633af25cfd7331140027b4ab41
nerikara <nerikara4chimi@gmail.com>
2020-01-12 23:35:19 +0900

    fix: DAOの実装

--- src/main/java/com/example/demo/repository/TaskDaoImpl.java
+++ src/main/java/com/example/demo/repository/TaskDaoImpl.java
@@ -18,30 +18,28 @@ import com.example.demo.entity.TaskType;
 public class TaskDaoImpl implements TaskDao {
 
 	private final JdbcTemplate jdbcTemplate;
-	
+
 	@Autowired
 	public TaskDaoImpl(JdbcTemplate jdbcTemplate) {
 		this.jdbcTemplate = jdbcTemplate;
 	}
-	
+
 	@Override
 	public List<Task> findAll() {
-		
+
 		String sql = "SELECT task.id, user_id, type_id, title, detail, deadline, "
 				+ "type, comment FROM task "
 				+ "INNER JOIN task_type ON task.type_id = task_type.id";
-		
-		//削除してください
-		
+
 		//タスク一覧をMapのListで取得
-		List<Map<String, Object>> resultList = null;
-		
+		List<Map<String, Object>> resultList = jdbcTemplate.queryForList(sql);
+
 		//return用の空のListを用意
-		List<Task> list = null;
-		
+		List<Task> list = new ArrayList<Task>();
+
 		//二つのテーブルのデータをTaskにまとめる
 		for(Map<String, Object> result : resultList) {
-			
+
 			Task task = new Task();
 			task.setId((int)result.get("id"));
 			task.setUserId((int)result.get("user_id"));
@@ -49,29 +47,30 @@ public class TaskDaoImpl implements TaskDao {
 			task.setTitle((String)result.get("title"));
 			task.setDetail((String)result.get("detail"));
 			task.setDeadline(((Timestamp) result.get("deadline")).toLocalDateTime());
-			
+
 			TaskType type = new TaskType();
 			type.setId((int)result.get("type_id"));
 			type.setType((String)result.get("type"));
 			type.setComment((String)result.get("comment"));
-			
+
 			//TaskにTaskTypeをセット
-			
+			task.setTaskType(type);
+
 			list.add(task);
 		}
 		return list;
 	}
-	
+
 	@Override
 	public Optional<Task> findById(int id) {
 		String sql = "SELECT task.id, user_id, type_id, title, detail, deadline, "
 				+ "type, comment FROM task "
 				+ "INNER JOIN task_type ON task.type_id = task_type.id "
 				+ "WHERE task.id = ?";
-		
+
 		//タスクを一件取得
-		Map<String, Object> result = null;
-		
+		Map<String, Object> result = jdbcTemplate.queryForMap(sql, id);
+
 		Task task = new Task();
 		task.setId((int)result.get("id"));
 		task.setUserId((int)result.get("user_id"));
@@ -79,18 +78,16 @@ public class TaskDaoImpl implements TaskDao {
 		task.setTitle((String)result.get("title"));
 		task.setDetail((String)result.get("detail"));
 		task.setDeadline(((Timestamp) result.get("deadline")).toLocalDateTime());
-		
+
 		TaskType type = new TaskType();
 		type.setId((int)result.get("type_id"));
 		type.setType((String)result.get("type"));
 		type.setComment((String)result.get("comment"));
 		task.setTaskType(type);
-		
-		//削除してください
-		Optional<Task> taskOpt = null;
-		
+
 		//taskをOptionalでラップする
-		
+		Optional<Task> taskOpt = Optional.ofNullable(task);
+
 		return taskOpt;
 	}
 
@@ -99,12 +96,11 @@ public class TaskDaoImpl implements TaskDao {
 		jdbcTemplate.update("INSERT INTO task(user_id, type_id, title, detail, deadline) VALUES(?, ?, ?, ?,?)",
 				 task.getUserId(), task.getTypeId(), task.getTitle(), task.getDetail(), task.getDeadline() );
 	}
-	
+
 	@Override
 	public int update(Task task) {
 		return jdbcTemplate.update("UPDATE task SET type_id = ?, title = ?, detail = ?,deadline = ? WHERE id = ?",
 				task.getTypeId(), task.getTitle(), task.getDetail(), task.getDeadline(), task.getId() );
-		
 	}
 
 	@Override
