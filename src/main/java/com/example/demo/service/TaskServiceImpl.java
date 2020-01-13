e1ad318c2501b50e134d1e35fbdfa08432534e3f
nerikara <nerikara4chimi@gmail.com>
2020-01-13 15:19:38 +0900

    fix: Service実装

--- src/main/java/com/example/demo/service/TaskServiceImpl.java
+++ src/main/java/com/example/demo/service/TaskServiceImpl.java
@@ -14,7 +14,7 @@ import com.example.demo.repository.TaskDao;
 @Service
 public class TaskServiceImpl implements TaskService {
 
-		
+
 	private final TaskDao dao;
 
 	@Autowired
@@ -26,16 +26,14 @@ public class TaskServiceImpl implements TaskService {
 	public List<Task> findAll() {
 		return dao.findAll();
 	}
-	
+
 	@Override
 	public Optional<Task> getTask(int id) {
-		
-		//削除してください
-		Optional<Task> taskOpt = null;
-		return taskOpt;
-		
-		//Optional<Task>一件を取得 idが無ければ例外発生　
-
+		try {
+			return dao.findById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new TaskNotFoundException("指定されたタスクが存在しません");
+		}
 	}
 
 	@Override
@@ -45,16 +43,16 @@ public class TaskServiceImpl implements TaskService {
 
 	@Override
 	public void update(Task task) {
-		
-		//Taskを更新　idが無ければ例外発生
-
+		if (dao.update(task) == 0) {
+			throw new TaskNotFoundException("更新するタスクが存在しません");
+		}
 	}
 
 	@Override
 	public void deleteById(int id) {
-		
-		//Taskを更新 idがなければ例外発生
-
+		if (dao.deleteById(id) == 0) {
+			throw new TaskNotFoundException("削除するタスクが存在しません");
+		}
 	}
 
 
